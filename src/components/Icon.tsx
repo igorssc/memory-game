1dd4c182a7ac62b1d21b81b74cea8a318340812d
Igor Santos <igorsantoscosta@gmail.com>
2023-01-07 01:02:33 -0300

    Edit grid icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -27,7 +27,7 @@ export const Icon = ({
     <>
       <span
         className={clsx(
-          "w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 rounded flex items-center justify-center cursor-pointer hover:scale-110 relative perspective-10"
+          "w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 rounded flex items-center justify-center cursor-pointer hover:scale-110 relative perspective-10"
         )}
         {...props}
       >
